2961292d1d9dda3c9d74d02cb522133d5a2787af
Szybet <53944559+Szybet@users.noreply.github.com>
2023-07-09 20:50:05 +0200

    changes - qrc file cmake and no code because problems

--- scripts/deploy_debug.sh
+++ scripts/deploy_debug.sh
@@ -5,9 +5,16 @@ cd ../
 servername="root@10.42.0.28"
 passwd="root"
 
+sshpass -p $passwd ssh $servername "bash -c \"killall -9 exec\""
+sshpass -p $passwd ssh $servername "bash -c \"killall -9 exec\""
+
 sshpass -p $passwd ssh $servername "bash -c \"killall -9 nachat\""
 sshpass -p $passwd ssh $servername "bash -c \"killall -9 nachat\""
 
 sshpass -p $passwd ssh $servername "bash -c \"rm /kobo/tmp/exec\""
+sshpass -p $passwd ssh $servername "bash -c \"rm /kobo/lib/libereaderdev.so*\""
+
 sshpass -p $passwd scp build/src/nachat $servername:/kobo/tmp/exec
+sshpass -p $passwd scp deps/ereaderdev-lib/ereaderdev/build/* $servername:/kobo/lib/
+
 sshpass -p $passwd ssh $servername "bash -c \"chroot /kobo /launch_app.sh\""
